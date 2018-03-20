99a22747f85833238ec884fefcdfdf857f52dffd
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-03-19 19:19:17 -0500

    Updated unit test

--- Tests/JNITests/JNITests.swift
+++ Tests/JNITests/JNITests.swift
@@ -19,20 +19,59 @@ class JNITests: XCTestCase {
     func testMethodSignature() {
         
         let testData: [(JNIMethodSignature, String)] = [
-            (JNIMethodSignature(argumentTypes: [], returnType: .void),
+            
+            (JNIMethodSignature(
+                argumentTypes: [],
+                returnType: .void),
              "()V"), // void f1()
-            (JNIMethodSignature(argumentTypes: [.int, .long], returnType: .int),
+            
+            (JNIMethodSignature(
+                argumentTypes: [
+                    .int,
+                    .long
+                ],
+                returnType: .int),
              "(IJ)I"), // int f2(int n, long l)
-            (JNIMethodSignature(argumentTypes: [.array(.int)], returnType: .boolean),
+            
+            (JNIMethodSignature(
+                argumentTypes: [
+                    .array(.int)
+                ],
+                returnType: .boolean),
              "([I)Z"), // boolean f3(int[] arr)
-            (JNIMethodSignature(argumentTypes: [.object(.java("lang", "String")), .int], returnType: .double),
+            
+            (JNIMethodSignature(
+                argumentTypes: [
+                    .object(.java("lang", "String")),
+                    .int
+                ],
+                returnType: .double),
              "(Ljava/lang/String;I)D"), // double f4(String s, int n)
-            (JNIMethodSignature(argumentTypes: [.int, .array(.object(.java("lang", "String"))), .char], returnType: .void),
+            
+            (JNIMethodSignature(
+                argumentTypes: [
+                    .int,
+                    .array(.object(.java("lang", "String"))),
+                    .char
+                ], returnType: .void),
              "(I[Ljava/lang/String;C)V"), // void f5(int n, String[] arr, char c)
-            (JNIMethodSignature(argumentTypes: [.int, .object(.java("lang", "String")), .array(.int)], returnType: .long),
+            
+            (JNIMethodSignature(
+                argumentTypes: [
+                    .int,
+                    .object(.java("lang", "String")),
+                    .array(.int)
+                ],
+                returnType: .long),
              "(ILjava/lang/String;[I)J"), // long f6(int n, String s, int[] arr)
-            (JNIMethodSignature(argumentTypes: [.object(.java("lang", "Boolean")), .boolean], returnType: .object(.java("lang", "String"))),
+            
+            (JNIMethodSignature(
+                argumentTypes: [
+                    .object(.java("lang", "Boolean")),
+                    .boolean],
+                returnType: .object(.java("lang", "String"))),
              "(Ljava/lang/Boolean;Z)Ljava/lang/String;"), // String f7(Boolean b, boolean b2)
+            
             (JNIMethodSignature(
                 argumentTypes: [
                     .object(["android", "content", "Context"]),
